a07620ab96c24588c186f4682312fedccd930e62
Muhammad Hassaan <31187421+hassaanhameed786@users.noreply.github.com>
2020-04-08 00:00:35 +0500

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,23 @@
+## Object  Oriented Programming 
+## Classes & Instances 
+
+import datetime
+
+class Person:
+      def __init__(self, name, age):
+           self.name = name
+           self.age = age
+           
+
+
+
+p1 = Person("hassy", 36)
+## Instance od class
+print(p1.name)
+print(p1.age) 
+
+
+
+
+
+
